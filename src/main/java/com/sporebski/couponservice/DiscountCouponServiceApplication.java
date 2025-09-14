0e677fcb1d13d68c5662b0c530ede115203cfa8e
Szymon <szymonporebski@hotmail.com>
2025-09-14 21:32:04 +0200

    feat: initial setup of Spring Boot project

---
+++ src/main/java/com/sporebski/couponservice/DiscountCouponServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sporebski.couponservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscountCouponServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscountCouponServiceApplication.class, args);
+    }
+
+}
